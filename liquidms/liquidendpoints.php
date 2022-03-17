f5ddbca88c3e69a99ee2dde3acb4039c43cbc60b
zibonbadi <zibonbadi@users.noreply.github.com>
2022-03-17 21:47:05 +0100

    Very very very basic server browser

--- liquidms/liquidendpoints.php
+++ liquidms/liquidendpoints.php
@@ -10,6 +10,7 @@ $router->with('/liquidms', function() use ($router){
 	$router->respond('GET', '/?', function($request, $response, $service){
 		$response->json([
 		"routes" => [
+			"/browse",
 			"/license",
 			"/snitch",
 			]
@@ -18,7 +19,20 @@ $router->with('/liquidms', function() use ($router){
 
 	$router->respond('GET', '/license/?', function($request, $response, $service){
 		$response->header('Content-Type','text/plain;syntax=markdown');
-		return file_get_contents(__DIR__."/LICENSE.md");
+		return file_get_contents(__DIR__."/../LICENSE.md");
+	});
+
+	$router->respond('GET', '/browse', function($request, $response, $service){
+			// Server test kludge. The game seems to ping every listed server and
+			// filter by response. Listing dummy servers is thus not possible.
+			$servers = NetgameModel::getServers();
+			$rooms = NetgameModel::getRooms();
+				if( ($servers["error"] == 0) && ($rooms["error"] == 0) ){ $service->render(__DIR__."/modules/HtmlView.php", ["data" => $servers, "rooms" => $rooms]);
+				}else{
+					$response->code(403);
+					if( ($servers["error"] != 0)){ $service->render(__DIR__."/modules/ErrorView.php", ["response" => $servers]); };
+					if( ($rooms["error"] != 0)){ $service->render(__DIR__."/modules/ErrorView.php", ["response" => $rooms]); };
+				}
 	});
 
 	$router->respond('PUT', '/?', function($request, $response){
