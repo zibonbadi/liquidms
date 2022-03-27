7f2eaba40405a19a37bdb3d4a04b1079145a8653
zibonbadi <zibonbadi@users.noreply.github.com>
2022-03-27 13:50:34 +0200

    Prep better server browser

---
+++ liquidms/frontend.php
@@ -0,0 +1,51 @@
+<?php
+# liquidMS - distributable SRB2 master server
+# Copyright (C) 2021-2022 Zibon Badi et al.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+require_once __DIR__.'/modules/ConfigModel.php';
+require_once __DIR__.'/modules/NetgameModel.php';
+
+use LiquidMS\ConfigModel;
+use LiquidMS\NetgameModel;
+
+$router->with('/liquidms/browse', function() use ($router){
+	$router->respond('GET', '/?', function($request, $response, $service){
+			$servers = NetgameModel::getServers();
+			$rooms = NetgameModel::getRooms();
+				if( ($servers["error"] == 0) && ($rooms["error"] == 0) ){ $service->render(__DIR__."/modules/HtmlView.php", ["data" => $servers, "rooms" => $rooms]);
+				}else{
+					$response->code(403);
+					if( ($servers["error"] != 0)){ $service->render(__DIR__."/modules/ErrorView.php", ["response" => $servers]); };
+					if( ($rooms["error"] != 0)){ $service->render(__DIR__."/modules/ErrorView.php", ["response" => $rooms]); };
+				}
+	});
+	// Three separate resource routes for capsuled security
+	$router->respond('GET', '/static/[:path]', function($request, $response, $service){
+			$response->header('Content-type', 'text/html');
+			$response->file(__DIR__."/../public/static/".$request->path);
+	});
+	$router->respond('GET', '/css/[:path]', function($request, $response, $service){
+			$response->header('Content-type', 'text/css');
+			$response->file(__DIR__."/../public/css/".$request->path);
+	});
+	$router->respond('GET', '/js/[:path]', function($request, $response, $service){
+			$response->header('Content-type', 'application/javascript');
+			$response->file(__DIR__."/../public/js/".$request->path);
+	});
+});
+
+?>
+
--- liquidms/modules/HtmlView.php
+++ liquidms/modules/HtmlView.php
@@ -19,55 +19,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
-:root{
-	--color-bg: teal;
-	--color-text: yellow;
-	--color-table-bg: #003;
-	--color-table-bg2: #005;
-	--color-table-bg-head: #007;
-	--color-table-bg-hover: #335;
-	--color-table-border: #444;
-}
-
-@media screen and (prefers-color-scheme: dark){
-:root{
-	--color-bg: #000;
-	--color-text: yellow;
-	--color-table-bg: #003;
-	--color-table-bg2: #005;
-	--color-table-bg-head: #007;
-	--color-table-bg-hover: #335;
-	--color-table-border: #444;
-}
-}
-
-body{
-color: var(--color-text);
-background: var(--color-bg);
-font-family: 'Press Start 2P', sans-serif;
-}
-
-h1,p{
-max-width: 60em;
-}
-
-table,tr,td{ border: none; }
-
-table{
-	background: var(--color-table-bg);
-	border-color: var(--color-table-border);
-	margin-left: auto;
-	margin-right: auto;
-}
-
-td,th{ padding: 0.75em; }
-tr:first-child{ background: var(--color-table-bg-head); }
-tr:nth-child(2n){ background: var(--color-table-bg2); }
-tr:hover{ background: var(--color-table-bg-hover); }
-
-</style>
+<link rel="stylesheet" href="browse/css/main.css">
+<script src="browse/js/main.js" defer></script>
 </head>
 <body>
 <h1>Sorry you gotta see us nude!</h1>
@@ -79,7 +32,8 @@ $maincontent = "<table><tr>"
 				."<th>Title</th>"
 				."<th>Version</th>"
 				."<th>Room</th>"
-				."<th>Origin</th>";
+				."<th>Room</th>"
+				."<th>Last updated</th>";
 $import = $this->sharedData()->get('data');
 
 // Guarantee room slots
@@ -90,7 +44,8 @@ foreach($import["data"] as $import_index => $import_value){
 		."<td>".urldecode($import_value["servername"])."</td>"
 		."<td>".$import_value["version"]."</td>"
 		."<td>".$import_value["roomname"]."</td>"
-		."<td>".$import_value["origin"]."</td></tr>";
+		."<td>".$import_value["origin"]."</td>"
+		."<td>".$import_value["updated_at"]."</td></tr>";
 }
 $maincontent .= "</table>\n";
 echo $maincontent;
