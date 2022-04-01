54ba55012f23d6dbe0ee467bea523bf1b517c951
zibonbadi <zibonbadi@users.noreply.github.com>
2022-04-02 00:17:13 +0200

    Add fetch-from-snitch

--- liquidms/modules/fetch_common.php
+++ liquidms/modules/fetch_common.php
@@ -16,77 +16,141 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 function fetchUpdate(array $config, array $jobs = []){
+	$rVal = []; // Return value
+
+	foreach($jobs as $jobname => $jobval) {
+		echo "[".date(DateTime::ISO8601, time())." {$jobname}] Fetching \"{$jobval["host"]}\"...\n";
+		$sv_new = [];
+		$currentjob = $config["fetch"][$jobname];
+		#var_dump($jobval);
+		#var_dump($currentjob);
+		switch($jobval["api"]){
+		case "snitch":{ $sv_new = fetchUpdate_snitch($config ,$currentjob); break; }
+		case "v1":
+		default:{ $sv_new = fetchUpdate_v1($config ,$currentjob); break; }
+		}
+		$rVal = array_merge($rVal, $sv_new);
+	}
+
+   // Below: return value structure in YAML format (one server).
+   // Defaults and examples are noted in paretheses:
+   //
+   // ---
+   // - host: "[Server IP address (127.0.0.1)]"
+   //   port: [Port (5029)]
+   //   servername: "[Server name (SRB2%20server)]"
+   //   version: "[Server version (2.2.9)]"
+   //   roomname: "[Room name ("Casual", "World", etc.)]"
+   //   origin: "[Room origin (mb.srb2.org)]"
+   // ...
+   //
+   // The field "origin" is optional. If empty, it indicates a server
+   // registered to the node's world.
+   return $rVal;
+}
+
+function fetchUpdate_snitch(array $config, array $job = []){
+
+	$rVal = [];
+	if (($handle = fopen(rtrim($job["host"], "/")."/liquidms/snitch", "r")) !== FALSE) {
+		while(($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			$row = [];
+			$row["host"] = $data[0];
+			$row["port"] = $data[1];
+			$row["servername"] = $data[2];
+			$row["version"] = $data[3];
+			$row["roomname"] = $data[4];
+			$row["origin"] = $data[5];
+			$rVal[] = $row;
+		}
+		fclose($handle);
+	}
+	#var_dump($rVal);
+
+	// Below: return value structure in YAML format (one server).
+	// Defaults and examples are noted in paretheses:
+	//
+	// ---
+	// - host: "[Server IP address (127.0.0.1)]"
+	//   port: [Port (5029)]
+	//   servername: "[Server name (SRB2%20server)]"
+	//   version: "[Server version (2.2.9)]"
+	//   roomname: "[Room name ("Casual", "World", etc.)]"
+	//   origin: "[Room origin (mb.srb2.org)]"
+	// ...
+	//
+	// The field "origin" is optional. If empty, it indicates a server
+	// registered to the node's world.
+	return $rVal;
+
+}
+
+function fetchUpdate_v1(array $config, array $job = []){
    $rVal = []; // Return value
-   $response = "";
 
-   foreach($jobs as $jobname => $jobval) {
-	  echo "[".date(DateTime::ISO8601, time())." {$jobname}] Fetching \"{$jobval["host"]}\"...\n";
-      $currentjob = $config["fetch"][$jobname];
-      #var_dump($currentjob["host"]);
+   $res_rooms = file_get_contents(rtrim($job["host"], "/")."/rooms");
+   $res_server = file_get_contents(rtrim($job["host"], "/")."/servers");
 
-      $res_rooms = file_get_contents(rtrim($currentjob["host"], "/")."/rooms");
-      $res_server = file_get_contents(rtrim($currentjob["host"], "/")."/servers");
+   // Regex match room list into a group array:
+   // - - "[Entire match]"
+   //   - "[Room # as string]"
+   //   - "[Room name]"
+   //
+   // Regex explained:
+   // "/regex/m": multiline
+   // "([0-9]+)\n": Capture room number (line with sole number on it)
+   // "((?:[^\n]+\n)": Capture Room name
+   preg_match_all("/([0-9]+)\n([^\n]+)\n/m", $res_rooms, $rooms, PREG_SET_ORDER);
 
-      // Regex match room list into a group array:
-      // - - "[Entire match]"
-      //   - "[Room # as string]"
-      //   - "[Room name]"
-      //
-      // Regex explained:
-      // "/regex/m": multiline
-      // "([0-9]+)\n": Capture room number (line with sole number on it)
-      // "((?:[^\n]+\n)": Capture Room name
-      preg_match_all("/([0-9]+)\n([^\n]+)\n/m", $res_rooms, $rooms, PREG_SET_ORDER);
+   // Regex match server list into a group array:
+   // - - "[Entire match]"
+   //   - "[Room # as string]"
+   //   - "[Multiline server table]"
+   //
+   // Regex explained:
+   // "/regex/m": multiline
+   // "([0-9]+)\n": Capture room number (line with sole number on it)
+   // "((?:.*\n)*?": Capture server lines as block; Lines require additional filtering
+   // "(?:\n|\Z)": Don't capture; match end of block (either \n or EOT)
+   preg_match_all('/^([0-9]+)\n((?:.*\n)*?(?:\n|\Z))/m', $res_server, $servers, PREG_SET_ORDER);
 
-      // Regex match server list into a group array:
-      // - - "[Entire match]"
-      //   - "[Room # as string]"
-      //   - "[Multiline server table]"
-      //
-      // Regex explained:
-      // "/regex/m": multiline
-      // "([0-9]+)\n": Capture room number (line with sole number on it)
-      // "((?:.*\n)*?": Capture server lines as block; Lines require additional filtering
-      // "(?:\n|\Z)": Don't capture; match end of block (either \n or EOT)
-      preg_match_all('/^([0-9]+)\n((?:.*\n)*?(?:\n|\Z))/m', $res_server, $servers, PREG_SET_ORDER);
+   foreach($servers as $roomid =>  $roomdata){
+	   // Break Server lists into Lines and feed the array
 
-      foreach($servers as $roomid =>  $roomdata){
-	 // Break Server lists into Lines and feed the array
+	   // Filter server block into distinct value arrays (step 2)
+	   // - - "[server line]"
+	   //   - "[IP]"
+	   //   - "[port]"
+	   //   - "[name]"
+	   //   - "[version]"
+	   $serversplit = explode("\n",rtrim($roomdata[2],"\n"));
 
-	 // Filter server block into distinct value arrays (step 2)
-	 // - - "[server line]"
-	 //   - "[IP]"
-	 //   - "[port]"
-	 //   - "[name]"
-	 //   - "[version]"
-	 $serversplit = explode("\n",rtrim($roomdata[2],"\n"));
+	   foreach($serversplit as $rowid =>  $rowdata){
+		   $newrow = [];
+		   $rowfields = explode(" ",$rowdata);
 
-	 foreach($serversplit as $rowid =>  $rowdata){
-	    $newrow = [];
-	    $rowfields = explode(" ",$rowdata);
+		   // Figure out server name
+		   $roomname = "Dummy name";
+		   foreach($rooms as $r_infoid =>  $r_infodata){
+			   if($roomdata[1] == $r_infodata[1]){
+				   $roomname = $r_infodata[2];
+				   break;
+			   }
+		   }
 
-	    // Figure out server name
-	    $roomname = "Dummy name";
-	    foreach($rooms as $r_infoid =>  $r_infodata){
-	       if($roomdata[1] == $r_infodata[1]){
-		  $roomname = $r_infodata[2];
-		  break;
-	       }
-	    }
+		   // Build return value conforming entry
+		   $newrow["host"] = $rowfields[0];
+		   $newrow["port"] = intval($rowfields[1]);
+		   $newrow["servername"] = $rowfields[2];
+		   $newrow["version"] = $rowfields[3];
+		   $newrow["roomname"] = $roomname;
+		   $newrow["origin"] = parse_url($job["host"])["host"]; // Extract hostname from URL
 
-	    // Build return value conforming entry
-	    $newrow["host"] = $rowfields[0];
-	    $newrow["port"] = intval($rowfields[1]);
-	    $newrow["servername"] = $rowfields[2];
-	    $newrow["version"] = $rowfields[3];
-	    $newrow["roomname"] = $roomname;
-	    $newrow["origin"] = parse_url($currentjob["host"])["host"]; // Extract hostname from URL
-
-	    // Insert entry
-	    $rVal[] = $newrow;
-	 }
-      }
+		   // Insert entry
+		   $rVal[] = $newrow;
+	   }
    }
+
 
    // Below: return value structure in YAML format (one server).
    // Defaults and examples are noted in paretheses:
