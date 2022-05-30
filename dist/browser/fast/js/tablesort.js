0b5aa9d2126e952d62b2fd481233342b372ff1f2
zibonbadi <zibonbadi@users.noreply.github.com>
2022-05-30 19:58:42 +0200

    Simplify table sorter

--- dist/browser/fast/js/tablesort.js
+++ dist/browser/fast/js/tablesort.js
@@ -1,35 +1,21 @@
-document.querySelector('thead').addEventListener('click', (e) => {
-	var table, rows, has_switched, i, x, y, to_switch, sortRow;
-	sortRow = event.target.cellIndex;
-	table = document.querySelector("table");
-	has_switched = true;
-	/*Make a loop that will continue until
-	no has_switched has been done:*/
-	while (has_switched) {
-	//start by saying: no switching is done:
-	has_switched = false;
-	rows = table.rows;
-	// Iterare rows
-	for (i = 1; i < (rows.length - 1); i++) {
-	  //start by saying there should be no has_switched:
-	  to_switch = false;
-	  /*Get the two elements you want to compare,
-	  one from current row and one from the next:*/
-	  x = rows[i].querySelectorAll("TD")[sortRow];
-	  y = rows[i + 1].querySelectorAll("TD")[sortRow];
-	  //check if the two rows should switch place:
-	  if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-		//if so, mark as a switch and break the loop:
-		to_switch = true;
-		break;
-	  }
-	}
-	if (to_switch) {
-	  /*If a switch has been marked, make the switch
-	  and mark that a switch has been done:*/
-	  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-	  has_switched = true;
-	}
-	}
+const tables = document.querySelectorAll('table');
 
-});
+for (let tab of tables) {
+  const tbody = tab.tBodies[0];
+  const rows = Array.from(tbody.rows);
+  const headerCells = tab.tHead.rows[0].cells;
+
+  for (let th of headerCells) {
+    const column = th.cellIndex;
+
+    th.addEventListener('click', () => {
+      rows.sort((tr1, tr2) => {
+        const tr1Text = tr1.cells[column].textContent;
+        const tr2Text = tr2.cells[column].textContent;
+        return tr1Text.localeCompare(tr2Text);
+      });
+
+      tbody.append(...rows);
+    });
+  }
+}
