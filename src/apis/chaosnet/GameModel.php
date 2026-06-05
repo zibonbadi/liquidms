b14414e43dfbb0453521451a6250795524b9311c
zibonbadi <zibonbadi@users.noreply.github.com>
2026-06-05 21:24:56 +0200

    ChaosNet.GameModel: Switch URL encoding to RFC3986

--- src/apis/chaosnet/GameModel.php
+++ src/apis/chaosnet/GameModel.php
@@ -34,7 +34,7 @@ class GameModel{
 		$name = urldecode($name);
 		$name = preg_replace('/[[:cntrl:]]/', '', $name);
 		$name = mb_convert_encoding($name, 'UTF-8', 'UTF-8');
-		$name = urlencode($name);
+		$name = rawurlencode($name);
 		return $name;
 	}
 
